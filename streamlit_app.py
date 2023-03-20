91bab28e144bbe03f83f45debdfa44c9c1755e69
rafasgithub <128161729+rafasgithub@users.noreply.github.com>
2023-03-20 11:12:38 +0100

    Update streamlit_app.py
    append into dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,3 +37,8 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
+
+# add fruit to dataframe
+added_fruit = streamlit.text_input('What fruit would you like to add?')
+streamlit.dataframe(my_data_row).append(added_fruit)
+streamlit.text_input('Thanks for adding',added_fruit)
